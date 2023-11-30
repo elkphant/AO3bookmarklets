ecbfb93301105a54fa2cb5724ef14e63f56062ad
elkphant <150316875+elkphant@users.noreply.github.com>
2023-12-01 01:42:16 +0800

    replace X with Y.js

--- replace X with Y.js
+++ replace X with Y.js
@@ -1,6 +1,6 @@
 javascript:(function(){
     let b = prompt("Enter the word you want to change:", "");
-    let c = prompt("nter the word after making changes:", "");
+    let c = prompt("Enter the word after making changes:", "");
     let a = document.getElementById("workskin").innerHTML;
     a = a.replaceAll(new RegExp(b, "ig"), c);
     document.getElementById("workskin").innerHTML = a;
