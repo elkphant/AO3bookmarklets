f509342c554572ff71e358b7036b196ae021dd3d
elkphant <150316875+elkphant@users.noreply.github.com>
2023-12-03 17:51:54 +0800

    Update delete blanks.js

--- delete blanks.js
+++ delete blanks.js
@@ -1,6 +1,7 @@
+/*僅刪除&nbsp;與全形空白*/
+
 javascript:(function(){
-    var elements = document.querySelectorAll('#workskin p');
-    elements.forEach(function(e) {
-        e.innerHTML = e.innerHTML.replace(/&nbsp;|　/g, ' ');
-    });
+    var e=document.querySelectorAll('#workskin p');
+    e.forEach(function(e){
+        e.innerHTML=e.innerHTML.replace(/&nbsp;|　/g,' ')})
 })();
