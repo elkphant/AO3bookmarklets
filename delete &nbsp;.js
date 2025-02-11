6ec0e545426b9cbe168cdc86a5d17f092f74d696
elkphant <150316875+elkphant@users.noreply.github.com>
2025-02-11 22:27:04 +0800

    Update delete &nbsp;.js

--- delete &nbsp;.js
+++ delete &nbsp;.js
@@ -1,10 +1,9 @@
-/*delete blanks "&nbsp;" and replace <br/> with </p><p>
-刪除"&nbsp;"空白，以段落取代<br/>換行符號*/
-
+/*delete blanks and replace <br/> with </p><p>
+刪除空白，以段落取代<br/>換行符號*/
 javascript:(function(){
-  var e=document.getElementById('workskin');
-  if(e){
-    var t=e.innerHTML;
-    var n=t.replace(/&nbsp;/g,'').replaceAll(/<br\s*\/?>/ig,%27</p><p>%27);
-      e.innerHTML=%27<p>%27+n+%27</p>%27}
+  var e=document.querySelectorAll('#workskin p');
+    e.forEach(function(t){t.innerHTML=t.innerHTML.replace(/&nbsp;|\u3000/g,' ')});
+  var n=document.getElementById('workskin');
+  if(n){
+    n.innerHTML=n.innerHTML.replace(/<br\s*\/?>/gi,'</p><p>');}
 })();
